b822c4f23da25f7f8a481827b16f6a049da7ec36
michael <michael@dogkat.com>
2015-05-03 22:45:54 -0500

    Being cleanup (test on non-windows for now)

---
+++ life2.hpp
@@ -0,0 +1,138 @@
+/*
+ * life2.hpp
+ *
+ *  Created on: May 3, 2015
+ *      Author: michael
+ */
+
+#ifndef LIFE2_HPP_
+#define LIFE2_HPP_
+
+#include <vector>
+#include <iostream>
+
+/*----------------------------------------------------------
+                          MACROS
+----------------------------------------------------------*/
+#define MAX_STATES 10 /* maximum number of cell states */
+
+/*----------------------------------------------------------
+                          TYPES
+----------------------------------------------------------*/
+
+typedef __int8            int8_t;
+typedef __int16           int16_t;
+typedef __int32           int32_t;
+typedef __int64           int64_t;
+typedef unsigned __int8   uint8_t;
+typedef unsigned __int16  uint16_t;
+typedef unsigned __int32  uint32_t;
+typedef unsigned __int64  uint64_t;
+
+typedef uint8_t game_type;    /* Game type (conways, wireworld, etc.) */
+enum{
+	CONWAYS_LIFE,
+	BRIANS_BRAIN,
+	WIRE_WORLD,
+	LANGTONS_ANT
+};
+
+enum{
+	COLOR_RED = 0,
+	COLOR_GREEN = 1,
+	COLOR_BLUE = 2,
+	NUM_COLORS
+};
+
+enum{
+	STATE_0 = 0,
+	STATE_1,
+	STATE_2,
+	STATE_3,
+	STATE_4,
+	STATE_5,
+	STATE_6,
+	STATE_7,
+	STATE_8,
+	STATE_9,
+	NUM_STATES
+};
+
+class board_preferences {
+public:
+	board_preferences( std::string& _name, game_type _gametype, cartesian_range _terrain );
+	~board_preferences();
+	uint8_t getNumStates();
+	uint8_t* getCharacters();
+	uint8_t** getColors();
+	uint32_t getHeight();
+	uint32_t getWidth();
+	int getLowerXRange();
+	int getLowerYRange();
+	int getUpperXRange();
+	int getUpperYRange();
+
+
+private:
+	std::string& name; /* simulation name */
+	game_type gametype;  /* gametype */
+	uint8_t characters[NUM_STATES];/* characters */
+	uint8_t colors[NUM_STATES][NUM_COLORS];/* colors */
+	cartesian_range terrain;
+	const uint8_t num_states;
+
+};
+
+typedef struct {
+	int x;
+	int y;
+
+} cartesian;
+
+typedef struct {
+	int l_x;
+	int l_y;
+	int u_x;
+	int u_y;
+
+} cartesian_range;
+
+/*----------------------------------------------------------
+                         CLASSES
+----------------------------------------------------------*/
+class Board {                /* board object type */
+
+private:
+	std::string name;
+	board_preferences preferences;
+	std::vector<std::vector<Cell>> cells;
+
+public:
+	Board( board_preferences& _preferences );
+	virtual ~Board();
+
+	void generateAut( std::ostream& output );
+	void generateASCII( std::ostream& output, cartesian_range& window );
+};
+
+class Cell {                 /* Cell object type */
+
+public:
+	Cell( uint8_t _state ); /* Constructor */
+	virtual ~Cell(); /* Destructor */
+
+	/* Methods */
+	uint8_t getState();
+	void setState( uint8_t _state );
+
+private:
+	uint8_t state;
+};
+
+/*----------------------------------------------------------
+                        PROCEDURES
+----------------------------------------------------------*/
+
+
+
+#endif /* LIFE2_HPP_ */
---
+++ lifeclasses2.cpp
@@ -0,0 +1,209 @@
+/*
+ * lifeclasses2.cpp
+ *
+ *  Created on: May 3, 2015
+ *      Author: michael
+ */
+#include <string>
+#include "life2.hpp"
+
+/*----------------------------------------------------------
+             BOARD CONNSTRUCTORS/DESTRUCTORS
+----------------------------------------------------------*/
+Board::Board(board_preferences& _preferences) {
+	this->preferences = _preferences;
+
+}
+
+Board::~Board() {
+
+}
+
+/*----------------------------------------------------------
+                     BOARD METHODS
+----------------------------------------------------------*/
+void Board::generateAut( std::ostream& output ) {
+	bool lineflag = false;
+	uint8_t state = 1;
+
+	/* File is open, now write initial comments */
+	output << "# This file is an auto generated .aut of a Board object.\n#\n";
+	output << "# Generation created by Michael Johnson's 229 Project 2\n#\n\n";
+
+	/* Add Keywords and corresponding values */
+	output << "Name \"" << this->name << "\";\n\n";
+	output << "Xrange " << this->preferences.terrain.l_x << " "
+			<< this->preferences.terrain.u_x << ";\n\n";
+	output << "Yrange " << this->preferences.terrain.l_y << " "
+			<< this->preferences.terrain.u_y << ";\n\n";
+
+	/* Specify game_type */
+	switch(this->preferences.gametype){
+		case CONWAYS_LIFE:
+			output << "Rules ConwaysLife;\n\n";
+			break;
+		case BRIANS_BRAIN:
+			output << "Rules BriansBrain;\n\n";
+			break;
+		case WIRE_WORLD:
+			output << "Rules WireWorld;\n\n";
+			break;
+		case LANGTONS_ANT:
+			output << "Rules LangtonsAnt;\n\n";
+			break;
+	}
+
+	/* Specify state chars */
+	output << "Chars ";
+	for( uint8_t i = 0; i < this->preferences.getNumStates(); i++) {
+		output << this->preferences.getCharacters()[i];
+		output << ( ( i + 1 ) != this->preferences.getNumStates() ) ? ";\n\n" : "";
+	}
+
+	/* Specify state colors */
+	output << "Colors ";
+	for( uint8_t i = 0; i < this->preferences.getNumStates(); i++) {
+		output << "(" << this->preferences.getColors()[i][COLOR_RED] << ", "
+			   << this->preferences.getColors()[i][COLOR_GREEN] << ", "
+			   << this->preferences.getColors()[i][COLOR_BLUE] << ")";
+		output << ( ( i + 1 ) != this->preferences.getNumStates() ) ? ";\n\n" : ")";
+	}
+
+	/* Keywords done, now add data TODO: Clean this up a bit. */
+	output << "Initial{\n";
+	for(int y = 0; y <= this->preferences.getHeight(); y++) {
+		lineflag = true; /* Reset every line to re-add Y=.. */
+
+		for(int x = 0; x <= this->preferences.getWidth(); x++) {
+			if(cells[x][y].getState() != 0) {
+				if(cells[x][y].getState() != state) {
+					state = cells[x][y].getState();
+					if(lineflag == false) output << ";\n";
+					output << "State " << state << ";\n";
+					lineflag = true;
+				}
+
+				if(lineflag == true){
+				lineflag = false;
+				output << "Y = " << y + this->preferences.getLowerYRange() << ": " << x + this->preferences.getLowerXRange();
+				}
+				else{
+					output << ", " << x + this->preferences.getLowerXRange();
+				}
+			}
+		}
+		if(lineflag == false) output << ";\n"; /* Only newline if we put something on this line */
+	}
+
+	/* Now that datapoints have been added, finalize the file with }; */
+	output << "};\n\n";
+
+}
+
+void Board::generateASCII( std::ostream& output, cartesian_range& window ) {
+
+	for( int y = window.u_y - this->preferences.getLowerYRange();
+			y >= window.l_y - this->preferences.getLowerYRange(); y-- ) {
+		for( int x = window.l_x - this->preferences.getLowerXRange();
+				x <= window.u_x - this->preferences.getLowerXRange(); x++ ) {
+			if( x <= this->preferences.getWidth() && x >= 0
+					&& y <= this->preferences.getHeight() && y >= 0 ) {
+					output << this->preferences.getCharacters()[ this->cells[x][y].getState() ];
+			}
+			else {
+				output << this->preferences.getCharacters()[ STATE_0 ];
+			}
+		}
+		/* Start next line */
+		output << "\n";
+	}
+
+}
+
+/*----------------------------------------------------------
+              CELL CONSTRUCTORS/DESTRUCTORS
+----------------------------------------------------------*/
+Cell::Cell(uint8_t _state) {
+	this->state = _state;
+
+}
+
+Cell::~Cell() {
+
+}
+
+/*----------------------------------------------------------
+                     CELL METHODS
+----------------------------------------------------------*/
+
+
+uint8_t Cell::getState() {
+	return this->state;
+}
+
+void Cell::setState( uint8_t _state ) {
+	this->state = _state;
+}
+
+/*----------------------------------------------------------
+           BOARD PREFS CONNSTRUCTORS/DESTRUCTORS
+----------------------------------------------------------*/
+board_preferences::board_preferences( std::string& _name, game_type _gametype, cartesian_range _terrain ) {
+	this->name = _name;
+	this->gametype = _gametype;
+	this->terrain = _terrain;
+	switch( gametype ) {
+		case CONWAYS_LIFE:
+			this->num_states = 2;
+			break;
+		case BRIANS_BRAIN:
+			this->num_states = 3;
+			break;
+		case WIRE_WORLD:
+			this->num_states = 4;
+			break;
+		case LANGTONS_ANT:
+			this->num_states = 10;
+			break;
+	}
+
+}
+
+/*----------------------------------------------------------
+                  BOARD PREFS METHODS
+----------------------------------------------------------*/
+uint8_t board_preferences::getNumStates() {
+	return this->num_states;
+}
+
+uint8_t* board_preferences::getCharacters() {
+	return this->characters;
+}
+
+uint8_t** board_preferences::getColors() {
+	return this->colors;
+}
+
+uint32_t board_preferences::getHeight() {
+	return( this->terrain.u_y - this->terrain.l_y );
+}
+
+uint32_t board_preferences::getWidth() {
+	return( this->terrain.u_x - this->terrain.l_x );
+}
+
+int board_preferences::getLowerXRange() {
+	return this->terrain.l_x;
+}
+
+int board_preferences::getLowerYRange() {
+	return this->terrain.l_y;
+}
+
+int board_preferences::getUpperXRange() {
+	return this->terrain.u_x;
+}
+
+int board_preferences::getUpperYRange() {
+	return this->terrain.u_y;
+}
